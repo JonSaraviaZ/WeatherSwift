a0d6d6dd2689fd60e0fc0cd57711ba02d43a0451
jonathan <jonathan.saravia.223@gmail.com>
2026-04-21 15:56:00 -0400

    se sube el código inicial

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,15 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            DayForecast()
+            DayForecast()
+            
+            /*VStack {
+                Text("Tue")
+                Image(systemName: "cloud.rain.fill")
+                    .foregroundStyle(Color.blue)
+                Text("High: 60")
+                Text("Low: 40")
+            }
+            .padding()*/
+        }
+    }
+}
+
+struct DayForecast: View {
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Mon")
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("High: 70")
+            Text("Low: 50")
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
