c0ba10a6e87a7bf19faee949a9c9af90f20a944f
jonathan <jonathan.saravia.223@gmail.com>
2026-04-26 21:43:48 -0400

    se genera lógica y estilos de una app básica del clima

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast()
-            DayForecast()
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50 )
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 50 )
             
             /*VStack {
                 Text("Tue")
@@ -26,13 +26,35 @@ struct ContentView: View {
 }
 
 struct DayForecast: View {
+    
+    let day: String //Para declarar una propiedad hay que usar la palabra clave let
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String // Una pequeña lógica con booleanos para que se depsliegue un icono de soleado o lluvioso, y también para que se le aplique un color amarillo o azul
+    {
+        isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        isRainy ? Color.blue : Color.yellow
+    }
+    
+    
     var body: some View {
         VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
+            Text(day)
+                .font(Font.headline) // Así puedo darle cierto estilo a los textos con fuentes
+            Image(systemName:iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5) //un espaciado con padding
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)// Así puedo darle cierto estilo a los textos con fuentes
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.medium)// Así puedo darle cierto estilo a los textos con fuentes
+                .foregroundStyle(Color.secondary) //También puedo utilizar de esta manera ciertos estilos de color a los textos
         }
         .padding()
     }
